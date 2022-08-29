8c6e182c70e1d9fe31bca6b593d402cb03da6582
wkuliah654 <78148438+wkuliah654@users.noreply.github.com>
2022-08-29 11:23:01 +0700

    AI Game 29/08/2022

---
+++ AIGame/Assets/Script/AICharacterBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AICharacterBehaviour : MonoBehaviour
+{
+    public GameObject target; // target yang dikejar
+    public float movingSpeed = 2f; // kecepatan berpindah
+    public float turnSpeed = 0.05f; // kecepatan berbelok
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 gapPosition = target.transform.position - this.transform.position; // Gap antara posisi AI dengan target
+        gapPosition = new Vector3(gapPosition.x, 0, gapPosition.z);
+        // nilai gap y dibuat 0 agar AI mengabaikan posisi atas dan bawah (Y) dari target dan hanya mengikuti arah ke kanan dan ke kiri (X dan Z)
+
+        Quaternion lookRotation = Quaternion.LookRotation(gapPosition);
+        // Rotasi untuk look atau melihat target
+
+        this.transform.rotation = Quaternion.Lerp(this.transform.rotation, lookRotation, turnSpeed);
+        // Membuat rotasi berubah secara smooth menggunakan fungsi lerp dari rotasi awal ke rotasi tujuan lookRotation
+
+        this.transform.Translate(Vector3.forward * movingSpeed * Time.deltaTime);
+        // Bergerak maju
+    }
+}
---
+++ AIGame/Assets/Script/PlayerBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    public float movingSpeed = 15; // kecepatan bergerak
+
+    // Update is called once per frame
+    void Update()
+    {
+        // jika keyboard ditekan
+        if (Input.GetKey(KeyCode.LeftArrow))
+            this.transform.Translate(Vector3.left * movingSpeed * Time.deltaTime, Space.World);
+        else if (Input.GetKey(KeyCode.RightArrow))
+            this.transform.Translate(Vector3.right * movingSpeed * Time.deltaTime, Space.World);
+        else if (Input.GetKey(KeyCode.UpArrow))
+            this.transform.Translate(Vector3.forward * movingSpeed * Time.deltaTime, Space.World);
+        else if (Input.GetKey(KeyCode.DownArrow))
+            this.transform.Translate(Vector3.back * movingSpeed * Time.deltaTime, Space.World);
+    }
+}
